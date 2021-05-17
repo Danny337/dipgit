808b01506de430308feb0651d0f2412557506deb
Daniel <dmd17734@gmail.com>
2021-05-17 21:01:45 +0300

    Оптимизация картинок

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -4,9 +4,9 @@ import Carousel from 'react-bootstrap/Carousel';
 import pc from '../../images/pctech.jpg';
 
 import photo1 from '../../images/chin1.jpg';
-import photo2 from '../../images/dudechin.jpg';
-import photo3 from '../../images/motherplat.jpg';
-import sliderpc from '../../images/sliderpc.jpg';
+import photo2 from '../../images/chinit2.jpg';
+import photo3 from '../../images/motherplat2.jpg';
+import sliderpc from '../../images/sliderpc2.jpg';
 import Services from '../../pages/Services';
 
 export default class Slider extends Component {
